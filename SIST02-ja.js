bab4c507d41523d3e1ac12c43ae966dbdad2ea79
otoneko <117816972+otnkmk8d@users.noreply.github.com>
2023-08-15 18:38:46 +0900

    Update SIST02-ja.js
    表現の修正
    フォーマット

--- SIST02-ja.js
+++ SIST02-ja.js
@@ -1,16 +1,31 @@
-javascript: const formattedDate = new Date(document.lastModified).toLocaleDateString("ja-JP", {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
-}).split("/").join("-");
-var today = new Date().toLocaleDateString("ja-JP", {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
-}).split("/").join("-");
-var result = ("[著者名]. "+ "“" + document.title + "”. " + "[Webサイトの名称]. "+ formattedDate + ". " + location.href + ", (参照 " + today + ").");
-navigator.clipboard.writeText(result).then(
-  () => {
-    alert("参考文献をコピーしました");
-  },
-);
+const formattedDate = new Date(document.lastModified)
+  .toLocaleDateString("ja-JP", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  })
+  .split("/")
+  .join("-");
+const today = new Date()
+  .toLocaleDateString("ja-JP", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  })
+  .split("/")
+  .join("-");
+const result =
+  "[著者名]. " +
+  "“" +
+  document.title +
+  "”. " +
+  "[Webサイトの名称]. " +
+  formattedDate +
+  ". " +
+  location.href +
+  ", (参照 " +
+  today +
+  ").";
+navigator.clipboard.writeText(result).then(() => {
+  alert("生成した参考文献をコピーしました");
+});
