35d0f6d9703a4d3d43f5ad6e6824d65bd0d5794e
otoneko <117816972+otnkmk8d@users.noreply.github.com>
2023-08-15 18:39:06 +0900

    Update SIST02-en.js

--- SIST02-en.js
+++ SIST02-en.js
@@ -1,16 +1,31 @@
-javascript: const formattedDate = new Date(document.lastModified).toLocaleDateString("ja-JP", {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
-}).split("/").join("-");
-var today = new Date().toLocaleDateString("ja-JP", {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
-}).split("/").join("-");
-var result = ("[Author Name]. "+ "“" + document.title + "”. " + "[Website Name]. "+ formattedDate + ". " + location.href + ", (accessed " + today + ").");
-navigator.clipboard.writeText(result).then(
-  () => {
-    alert("Copied a Reference");
-  },
-);
+const formattedDate = new Date(document.lastModified)
+  .toLocaleDateString("ja-JP", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  })
+  .split("/")
+  .join("-");
+const today = new Date()
+  .toLocaleDateString("ja-JP", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  })
+  .split("/")
+  .join("-");
+const result =
+  "[Author Name]. " +
+  "“" +
+  document.title +
+  "”. " +
+  "[Website Name]. " +
+  formattedDate +
+  ". " +
+  location.href +
+  ", (accessed " +
+  today +
+  ").";
+navigator.clipboard.writeText(result).then(() => {
+  alert("Generated a reference and copied it to the clipboard");
+});
