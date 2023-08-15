80ee8fc2fe5636668cfddf2606ae3a50cb43bd7a
otoneko <117816972+otnkmk8d@users.noreply.github.com>
2023-08-15 19:15:34 +0900

    Update Get-Website-Info.js

--- Get-Website-Info.js
+++ Get-Website-Info.js
@@ -1,16 +1,35 @@
-javascript: const formattedDate = new Date(document.lastModified).toLocaleDateString("ja-JP", {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
-}).split("/").join("-");
-var today = new Date().toLocaleDateString("ja-JP", {
-  year: "numeric",
-  month: "2-digit",
-  day: "2-digit",
-}).split("/").join("-");
-var result = ("URL: " + location.href + "\n" + "Title: " + document.title + "\n" + "Last Modified: " + formattedDate + "\n"+ "Today: " + today);
-navigator.clipboard.writeText(result).then(
-  () => {
-    alert(result+"\n"+"Copied website info");
-  },
-);
+const lastModified = document.lastModified;
+var formattedDate = new Date(lastModified)
+  .toLocaleString("ja-JP", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  })
+  .split("/")
+  .join("-");
+var today = new Date()
+  .toLocaleDateString("ja-JP", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  })
+  .split("/")
+  .join("-");
+var result =
+  "URL: " +
+  location.href +
+  "\n" +
+  "Title: " +
+  document.title +
+  "\n" +
+  "Last Modified: " +
+  formattedDate +
+  "\n" +
+  "Today: " +
+  today;
+navigator.clipboard.writeText(result).then(() => {
+  alert(result + "\n" + "Copied website info");
+});
