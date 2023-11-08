bb10c3bf8f6cd561a38a221446c23e20afd19685
otoneko <tomo962115@gmail.com>
2023-11-08 14:36:34 +0900

    Wikipedia

--- SIST02-en.js
+++ SIST02-en.js
@@ -1,32 +1,35 @@
 javascript:
-const formattedDate = new Date(document.lastModified)
-  .toLocaleDateString("ja-JP", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  })
-  .split("/")
-  .join("-");
-const today = new Date()
-  .toLocaleDateString("ja-JP", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  })
-  .split("/")
-  .join("-");
-const result =
-  "[Author Name]. " +
-  "“" +
-  document.title +
-  "”. " +
-  "[Website Name]. " +
-  formattedDate +
-  ". " +
-  location.href +
+const formattedDate =
+  new Date(document.lastModified)
+    .toLocaleDateString("ja-JP", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    })
+    .split("/")
+    .join("-") + ". ";
+
+const today =
   ", (accessed " +
-  today +
+  new Date()
+    .toLocaleDateString("ja-JP", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    })
+    .split("/")
+    .join("-") +
   ").";
+let pageTitle = document.title;
+let authorName = "[Author Name]. ";
+let WebsiteName = "[Website Name]. ";
+if (location.href.includes("wikipedia")) {
+  pageTitle = "“" + pageTitle.replace(" - Wikipedia", "") + "”. ";
+  authorName = "Wikipedia contributors. ";
+  WebsiteName = "Wikipedia. ";
+}
+const result =
+  authorName + pageTitle + WebsiteName + formattedDate + location.href + today;
 navigator.clipboard.writeText(result).then(() => {
   alert("Generated a reference and copied it to the clipboard");
 });
